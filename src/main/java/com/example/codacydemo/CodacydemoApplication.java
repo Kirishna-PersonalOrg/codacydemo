9bd60d67b73779c365e373b8d465885a77058d07
KirishnaanandNataraj <knatarajan@academian.com>
2024-09-29 17:43:40 +0530

    Initial Commit

---
+++ src/main/java/com/example/codacydemo/CodacydemoApplication.java
@@ -0,0 +1,14 @@
+package com.example.codacydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodacydemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodacydemoApplication.class, args);
+		System.out.println("Shri NatarajanKamakshi Namaha");
+	}
+
+}
---
+++ src/test/java/com/example/codacydemo/CodacydemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.codacydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodacydemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
